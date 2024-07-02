d43b351eee5f6a6235a0b2f1c646242dd684b154
Jaifran Hernandez <jaifranjose@gmail.com>
2024-07-02 12:20:16 -0400

    feat: Implement basic containerization with user namespaces

--- main.go
+++ main.go
@@ -1,9 +1,12 @@
-package main
+//build linux
+
+package main;
 
 import (
 	"fmt"
 	"os"
 	"os/exec"
+	"syscall"
 )
 
 //Docker 			run image 	<cmd> <params>
@@ -22,11 +25,25 @@ func main() {
 func run() {
 	fmt.Printf("Running %v\n", os.Args[2:])
 
+	cmd := exec.Command("/proc/self/exe", append([]string{"child"}, os.Args[2:]...)...)
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	cmd.SysProcAttr = &syscall.SysProcAttr{
+		CloneFlags: syscall.CLONE_NEWUTS
+	}
+	cmd.Run()
+}
+
+func child() {
+	fmt.Printf("Running %v\n", os.Args[2:])
+
+	syscall.Sethostname([]byte("docker"))
+
 	cmd := exec.Command((os.Args[2]), os.Args[3:]...)
 	cmd.Stdin = os.Stdin
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
-
 	cmd.Run()
 
 }
