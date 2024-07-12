0530ff751b919069e2e5e8437b8a978e1138bbad
Jaifran Hernandez <jaifranjose@gmail.com>
2024-07-12 01:01:53 -0400

    feat: Implement container environment with process isolation, cgroup management, and filesystem mounts

--- main.go
+++ main.go
@@ -1,51 +1,73 @@
-//build linux
+// build linux
 
-package main;
+package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"os"
 	"os/exec"
+	"path/filepath"
+	"strconv"
 	"syscall"
 )
 
-//Docker 			run image 	<cmd> <params>
-// go run main/go	run			<cmd> <params>
-
-
+// go run main.go run <cmd> <args>
 func main() {
 	switch os.Args[1] {
 	case "run":
 		run()
+	case "child":
+		child()
 	default:
 		panic("invalid command")
 	}
 }
 
 func run() {
-	fmt.Printf("Running %v\n", os.Args[2:])
+	fmt.Printf("Running %v \n", os.Args[2:])
 
 	cmd := exec.Command("/proc/self/exe", append([]string{"child"}, os.Args[2:]...)...)
 	cmd.Stdin = os.Stdin
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
 	cmd.SysProcAttr = &syscall.SysProcAttr{
-		CloneFlags: syscall.CLONE_NEWUTS,
+		Cloneflags:   syscall.CLONE_NEWUTS | syscall.CLONE_NEWPID | syscall.CLONE_NEWNS,
+		Unshareflags: syscall.CLONE_NEWNS,
 	}
-	cmd.Run()
+
+	must(cmd.Run())
 }
 
 func child() {
-	fmt.Printf("Running %v\n", os.Args[2:])
+	fmt.Printf("Running %v \n", os.Args[2:])
 
-	syscall.Sethostname([]byte("docker"))
+	cg()
 
-	cmd := exec.Command((os.Args[2]), os.Args[3:]...)
+	must(syscall.Sethostname([]byte("docker")))
+	must(syscall.Chroot("/home/apokryphos/ubuntufs"))
+	must(os.Chdir("/"))
+	must(syscall.Mount("proc", "proc", "proc", 0, ""))
+	must(syscall.Mount("thing", "mytemp", "tmpfs", 0, ""))
+
+	cmd := exec.Command(os.Args[2], os.Args[3:]...)
 	cmd.Stdin = os.Stdin
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
-	cmd.Run()
 
+	must(cmd.Run())
+
+	must(syscall.Unmount("proc", 0))
+	must(syscall.Unmount("thing", 0))
+}
+
+func cg() {
+	cgroups := "/sys/fs/cgroup/"
+	pids := filepath.Join(cgroups, "pids")
+	os.Mkdir(filepath.Join(pids, "liz"), 0755)
+	must(ioutil.WriteFile(filepath.Join(pids, "apokryphos/pids.max"), []byte("20"), 0700))
+	must(ioutil.WriteFile(filepath.Join(pids, "apokryphos/notify_on_release"), []byte("1"), 0700))
+	must(ioutil.WriteFile(filepath.Join(pids, "apokryphos/cgroup.procs"), []byte(strconv.Itoa(os.Getpid())), 0700))
 }
 
 func must(err error) {
